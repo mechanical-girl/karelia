c2c02434159d5e580f4d30c0af10d202849292b3
Pouncy Silverkitten <pouncy.sk@gmail.com>
2016-12-04 15:55:12 +0000

    Add spoof to karelia.

--- karelia.py
+++ karelia.py
@@ -1,17 +1,31 @@
+#!/usr/local/bin/python
+
 #############################################################
 #   Karelia library, by PouncySilverkitten                  #
 #   github.com/PouncySilverkitten/karelia                   #
 #                                                           #
+#   Functions                                               #
+#   - connectTo(room)                                       #
+#   - disconnect(websocketObject)                           #
+#   - parse(message)                                        #
+#   - changeNick(newNick)                                   #
+#   - spoof(message)                                        #
 #############################################################
 
 from websocket import create_connection
 import json
 import time
+import sys
 
-
+botName = ""
 startTime = time.time()
+startDate = time.strftime("%Y-%m-%d %H:%M:%S")
 paused = False
 helpMessage = ["This bot has not had a help message specified. This message was generated automatically."]
+
+def changeNick(nick):
+    global ws
+    ws.send(json.dumps({"type": "nick","data": {"name":nick}}))
 
 def getUptime():
     global startTime
@@ -33,7 +47,7 @@ def getUptime():
         upminutes += 1
         upticks -= 60
 
-    uptime = str(updays) + " days " + str(uphours) + " hours " + str(upminutes) + " minutes"
+    uptime = startDate + " (" + str(updays) + " days " + str(uphours) + " hours " + str(upminutes) + " minutes)"
     return uptime
 
 def send(message,parent=''):
@@ -42,49 +56,102 @@ def send(message,parent=''):
     if not paused:
         ws.send(json.dumps({'type': 'send', 'data': {'content': message, 'parent': parent}}))
     
-def connectTo(room,name):
+def connectTo(room):
     global ws
     global botName
-    botName = name
     ws = create_connection("wss://euphoria.io/room/"+room+"/ws")
-    ws.send(json.dumps({"type": "nick","data": {"name":name}}))
+    ws.send(json.dumps({"type": "nick","data": {"name":botName}}))
     return(ws)
+
+def disconnect(room):
+    room.close()
 
 
 def parse(incoming):
     global ws
     global paused
-    
-    packet = json.loads(incoming)
-    
-    if packet["type"] == "ping-event":
-        ws.send(json.dumps({'type': 'ping-reply', 'data': {'time': packet['data']['time']}}))
+    global botName
 
-    elif packet['type'] == "send-event" and packet['data']['content'] == '!ping':
-        send('Pong!',packet['data']['id'])
-    elif packet['type'] == "send-event" and packet['data']['content'] == '!ping @' + botName:
-        send('Pong!',packet['data']['id'])
-        
-    elif packet['type'] == "send-event" and packet['data']['content'] == '!uptime':
-        uptime = getUptime()
-        send('/me has been up since ' + uptime,packet['data']['id'])        
-    elif packet['type'] == "send-event" and packet['data']['content'] == '!uptime @' + botName:
-        uptime = getUptime()
-        send('/me has been up for ' + uptime,packet['data']['id'])
+    try:
 
-    elif packet['type'] == "send-event" and packet['data']['content'] == '!pause @' + botName:
-        send('/me has been paused',packet['data']['id'])
-        paused = True
-    elif packet['type'] == "send-event" and packet['data']['content'] == '!unpause @' + botName:
-        paused = False
-        send('/me has been unpaused',packet['data']['id'])
-        
-    elif packet['type'] == "send-event" and packet['data']['content'] == '!help @' + botName:
-        for message in helpMessage:
-            sending = message.replace('**sender**','@' + packet['data']['sender']['name'].replace(' ',''))
-            send(sending,packet['data']['id'])
+        packet = json.loads(incoming)
+
+        if packet["type"] == "ping-event":
+            ws.send(json.dumps({'type': 'ping-reply', 'data': {'time': packet['data']['time']}}))
+
+        elif packet['type'] == "send-event" and packet['data']['content'] == '!ping':
+            send('Pong!',packet['data']['id'])
+        elif packet['type'] == "send-event" and packet['data']['content'] == '!ping @' + botName:
+            send('Pong!',packet['data']['id']) 
             
-    else:
-        print(incoming)
+        elif packet['type'] == "send-event" and packet['data']['content'] == '!uptime':
+            uptime = getUptime()
+            send('/me has been up since ' + uptime,packet['data']['id'])      
+        elif packet['type'] == "send-event" and packet['data']['content'] == '!uptime @' + botName:
+            uptime = getUptime()
+            send('/me has been up since ' + uptime,packet['data']['id'])
+
+        elif packet['type'] == "send-event" and packet['data']['content'] == '!pause @' + botName:
+            send('/me has been paused',packet['data']['id'])
+            paused = True
+        elif packet['type'] == "send-event" and packet['data']['content'] == '!unpause @' + botName:
+            paused = False
+            send('/me has been unpaused',packet['data']['id'])
+            
+        elif packet['type'] == "send-event" and packet['data']['content'] == '!help @' + botName:
+            for message in helpMessage:
+                sending = message.replace('**sender**','@' + packet['data']['sender']['name'].replace(' ',''))
+                send(sending,packet['data']['id'])
+
+        elif packet['type'] == "send-event" and packet['data']['content'] == "!kill @" + botName:
+            send("Bot killed; will now exit.")
+                
+        else:
+             return(packet)
+
+        if packet["type"] == "ping-event":
+            handleType = "ping"
+        else:
+            handleType = str(packet['data']['content'].split()[0][1:])
+            
+        return(json.loads(json.dumps({"type": "handled", 'class':handleType})))
         
-    return(packet)
+    except Exception as error:
+        print("Parsing error from karelia.py: " + str(error))
+        return(json.dumps({"type": "error"}))
+
+def spoof(packet):
+    global ws
+    global paused
+    global botName
+
+    try:
+
+        if packet['type'] == "send-event" and '!ping' in packet['data']['content']:
+            send('Pong!',packet['data']['id']) 
+            
+        elif packet['type'] == "send-event" and '!uptime' in packet['data']['content']:
+            uptime = getUptime()
+            send('/me has been up since ' + uptime,packet['data']['id'])      
+
+        elif packet['type'] == "send-event" and '!pause' in packet['data']['content']:
+            send('/me has been paused',packet['data']['id'])
+            paused = True
+        elif packet['type'] == "send-event" and '!unpause' in packet['data']['content']:
+            paused = False
+            send('/me has been unpaused',packet['data']['id'])
+            
+        elif packet['type'] == "send-event" and  '!help' in packet['data']['content']:
+            for message in helpMessage:
+                sending = message.replace('**sender**','@' + packet['data']['sender']['name'].replace(' ',''))
+                send(sending,packet['data']['id'])
+
+        elif packet['type'] == "send-event" and  "!kill" in packet['data']['content']:
+            send("Bot killed; will now exit.")
+                
+        else:
+             return(json.dumps({'type':'failure','cause':'packet contents not spoofable'}))
+            
+    except Exception as error:
+        print("Spoofing error from karelia.py: " + str(error))
+        return(json.loads(json.dumps({"type": "error"})))
