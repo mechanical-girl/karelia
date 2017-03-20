4475a4a9873cf0455b54bbc680025e6cb391dc48
PouncySilverkitten <struan@duncan-wilson.co.uk>
2017-03-20 17:50:44 +0000

    Add files via upload

--- karelia.py
+++ karelia.py
@@ -1,10 +1,9 @@
 #!/usr/local/bin/python
 
-#############################################################
-#   Karelia library, by PouncySilverkitten                  #
-#   github.com/PouncySilverkitten/karelia                   #
-#                                                           #
-#############################################################
+"""
+Karelia is a library of functions for connecting a bot to the Heim chat
+platform at euphoria.io                                              
+"""
 
 from websocket import create_connection
 import json, time, sys, os
@@ -21,11 +20,20 @@ room = ""
 class KareliaException(Exception):
     pass
 
-def changeNick(nick=botName):
+def changeNick(nick = botName):
+    """
+    changeNick sends the `nick` command to Heim servers.
+
+    If a nick is passed in as an argument, it will change to that and change
+    the `botName` variable to the value passed as an argument (for resilience
+    against `!antighost`, amongst other reasons). If no nick is specified, it
+    will assume that the `botName` variable is the desired nick.
+    """
     conn.send(json.dumps({"type": "nick","data": {"name":nick}}))
     botName = nick
 
 def getUptime():
+    """Called by the `!uptime` command. Returns time since connect as string."""
     global startTime
     timeNow = time.time()
 
@@ -45,33 +53,75 @@ def getUptime():
         upminutes += 1
         upticks -= 60
 
-    uptime = startDate + " (" + str(updays) + " days " + str(uphours) + " hours " + str(upminutes) + " minutes)"
+    uptime = "{} ({} days {} hours {} minutes)".format(startDate, updays, uphours, upminutes)
     return uptime
 
-def send(message,parent='',packet=False):
+def send(message, parent = '', packet = False):
+    """
+    Sends the supplied message. The parent message can be specified.
+
+    Arguments are: the message to be sent, the id of the parent message, the
+    packet being replied to, and whether or not message is a complete packet.
+
+    - message:  either a complete packet, or the `['data']['content']` field
+    of one. If the former, the packet argument must be set to true.
+    - parent:   the id of the message being replied to. If not specified,
+    karelia will send the message as a new parent i.e. bottom-level message.
+    - packet:   if set to `True`, the first argument will be treated as a
+    complete packet.
+
+    `karelia.send('Top-level message')` will send that as a top-level message.
+    
+    `karelia.send('It's a reply!','02aa8y85m7hts')` will send that message as
+    a reply to the message with id `02aa8y85m7hts`.
+
+    `karelia.send({'type': 'log', 'data': {'n':1000}}, True)` will send a log
+    request for the thousand most recent messages posted to the room.
+    """
     if not paused:
-        if packet != False and len(message) > 0:
-            message = message.replace('***sender**','@' + packet['data']['sender']['name'].replace(' ',''))
-        conn.send(json.dumps({'type': 'send', 'data': {'content': message, 'parent': parent}}))
+        if packet:
+            conn.send(packet)
+        elif len(message) > 0:
+            conn.send(json.dumps({'type': 'send', 'data': {'content': message,
+                                                           'parent': parent}}))
     
 def connectTo(roomName):
+    """Connects to specified room and sets nick. Returns a connection object."""
     global conn, room
     room = roomName
-    conn = create_connection("wss://euphoria.io/room/"+roomName+"/ws")
+    conn = create_connection("wss://euphoria.io/room/{}/ws".format(roomName))
     conn.send(json.dumps({"type": "nick","data": {"name":botName}}))
     return(conn)
 
 def disconnect(conn):
+    """Attempts to close the connection passed to it."""
     try:
         conn.close()
     finally:
         return()
 
-def parse(packet = False, name=False):
+def parse(packet = False, name = False):
+    """
+    parse() handles the commands specified in the Botrulez
+    (github.com/jedevc/botrulez) and those required to stay alive.
+
+    parse() is a blocking function - that is, it will always wait until it
+    receives a packet from heim before returning.
+
+    On receiving a packet, it will reply to pings (both global and specific,
+    offer uptime, pause and unpause the bot, respond to help requests (again,
+    both global and local) and antighost commands, and kills the bot.
+
+    Regardless of actions taken, it will return the unaltered packet. If an
+    error occurs, it will return an exception.
+
+    Note: as of 2017-03-16 if killed, it will return sys.exit().
+    """
     global paused
     global lastMessage
-    if name == False:
-        name = botName
+    spoof = False
+    if name == False: name = botName
+    else: spoof = True
 
     try:
         incoming = conn.recv()
@@ -79,21 +129,23 @@ def parse(packet = False, name=False):
         if lastMessage != incoming:
             lastMessage = incoming
 
-            packet = json.loads(incoming)
+            if not packet: packet = json.loads(incoming)
 
-            if packet["type"] == "ping-event":
-                conn.send(json.dumps({'type': 'ping-reply', 'data': {'time': packet['data']['time']}}))
+            if packet["type"] == "ping-event" and not spoof:
+                conn.send(json.dumps({'type': 'ping-reply', 'data': {
+                        'time': packet['data']['time']}}))
 
             elif packet['type'] == "send-event":
                 
-                if packet['data']['content'] == '!ping':
+                if packet['data']['content'] == '!ping' and not spoof:
                     send('Pong!', packet['data']['id'])
                 if packet['data']['content'] == '!ping @{0}'.format(name):
                     send('Pong!',packet['data']['id']) 
          
                 if packet['data']['content'] == '!uptime @{0}'.format(name):
                     uptime = getUptime()
-                    send('/me has been up since ' + uptime,packet['data']['id'])
+                    send('/me has been up since {}'.format(uptime),packet[
+                            'data']['id'])
 
                 if packet['data']['content'] == '!pause @{0}'.format(name):
                     send('/me has been paused',packet['data']['id'])
@@ -104,33 +156,66 @@ def parse(packet = False, name=False):
                     
                 if packet['data']['content'] == '!help @{0}'.format(name):
                     for message in helpMessage:
-                        sending = message.replace('**sender**','@' + packet['data']['sender']['name'].replace(' ',''))
+                        sending = message.replace('**sender**','@{}'.format(
+                                packet['data']['sender']['name'].replace(' ',''))
                         send(sending,packet['data']['id'])
-                if packet['data']['content'] == '!help':
+                if packet['data']['content'] == '!help' and not spoof:
                     send(shortHelp,packet['data']['id'])
 
                 if packet['data']['content'] == '!kill @{0}'.format(name):
                     send("Bot killed; will now exit.",packet['data']['id'])
+                    log("Killed by user.",packet['data'])
                     return(sys.exit())
 
-                if packet['data']['content'] == "!antighost":
-                    changeNick(botName)
+                if packet['data']['content'] == "!antighost" and not spoof:
+                    changeNick()
 
             return(packet)
         
     except Exception as e:
-        raise KareliaException({'message':'There was an error parsing the message','error':e})
+        raise KareliaException(
+            {'message':'There was an error parsing the message','error':e})
 
-def spoof(packet,spoofBot):
-    try: parse(packet,spoofBot)      
+def spoof(packet, spoofBot):
+    """
+    spoof() takes two arguments (packet and spoofBot)
+
+    Calling spoof(packet, spoofBot) causes karelia to respond to the packet
+    offered as though it were named spoofBot.
+    """
+    try: parse(packet, spoofBot)      
     except Exception as e:
         print("Spoofing error from karelia.py: " + str(e))
         return({"type": "error",'error':str(e)})
 
-def log(e, message = ''):
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    exception = "Exception on message: {}\n{}: {}\n  {}\n  {}\n  {} at line {}\n\n".format(message['data'], type(e), e, exc_type, exc_obj, fname, exc_tb.tb_lineno)
+def log(error = '', message = ''):
+    """
+    logs as much information as possible to an external file.
+
+    log should be passed an exception object and if possible the message being
+    processed at the time of the exception. It will then write out as much as
+    it can about the exception to a logfile.
+    """
+    try:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        exception = """Exception on message: {}
+{}: {}
+    {}
+{}
+}} at line {}
+
+""".format(message['data'], type(error), error, exc_type, exc_obj, fname,
+           exc_tb.tb_lineno)
+                                                  
+    except Exception as e:
+        print(e)
+        exception = """Intelligent logging failed, falling back.
+{}
+{}
+
+""".format(error, message)
+                                                  
     with open("{} &{}.log".format(botName, room), 'a') as f:
         f.write(exception)
     print(exception)
