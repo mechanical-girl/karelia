a8010f8698d1be1383e0db861d8d282be328f475
PouncySilverkitten <struan@duncan-wilson.co.uk>
2018-02-12 18:02:26 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(  name='Karelia',
+        version='1.1',
+        description='Python library for euphoria.io',
+        author='Pouncy Silverkitten'
+        author_email='pouncy.sk@gmail.com',
+        url='https://github.com/pouncysilverkitten/karelia',
+        py_modules=['karelia'],)
