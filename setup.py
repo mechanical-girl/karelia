216695181d720f428420c343c400a9cfa46686d0
PouncySilverkitten <struan@duncan-wilson.co.uk>
2018-08-16 22:16:41 +0100

    Fix versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(  name='Karelia',
-        version='2.06',
+        version='2.0.6',
         description='Python library for euphoria.io',
         author='Pouncy Silverkitten',
         author_email='pouncy.sk@gmail.com',
