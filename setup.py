110cc4e139953061896b43dae45028f2c7a6d20e
PouncySilverkitten <pouncy.sk@gmail.com>
2018-08-23 20:50:33 +0100

    Prep for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(  name='Karelia',
         version='2.0.7',
         description='Python library for euphoria.io',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         author='Pouncy Silverkitten',
         author_email='pouncy.sk@gmail.com',
         url='https://github.com/pouncysilverkitten/karelia',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",],
         install_requires=[
             'websocket-client'
         ],
