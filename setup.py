3be3f618de9792f3d87164d5e5dea6fddf9093c6
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2022-04-05 13:38:13 +0100

    Updates

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 with open("README.md") as f:
     long_description = f.read()
 
-setup(  name='Karelia',
-        version='2.1.1',
+setup(  name='kareliapy',
+        version='2.2.1',
         description='Python library for euphoria.io',
         long_description=long_description,
         long_description_content_type="text/markdown",
         author='StruanDW',
         author_email='struan@duncan-wilson.co.uk',
-        url='https://github.com/pouncysilverkitten/karelia',
+        url='https://github.com/struandw/karelia',
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
